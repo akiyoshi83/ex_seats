23b2b67a7ce535c2601aac5d33c677ea9c8dcdcb
akiyoshi83 <mail@akiyoshi.info>
2015-08-07 22:46:38 +0900

    Fix assets load

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( development.js )
-Rails.application.config.assets.precompile += %w( development.css )
+
+if Rails.env.development?
+  Rails.application.config.assets.precompile += %w( development.js )
+  Rails.application.config.assets.precompile += %w( development.css )
+end
+
